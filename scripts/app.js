790676f03eb790177a0a1994c02785f49d919a75
Peter Toth <tothpeter08@gmail.com>
2016-02-14 20:08:57 +0100

    2.2 Creating a controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module('todoListApp', []);
+angular.module('todoListApp', [])
+.controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function() {
+    console.log('Hello there from the ctrl');
+  }
+});
