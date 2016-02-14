a2b8d9b71f2bd57939207413b882c26efb4c43f1
Peter Toth <tothpeter08@gmail.com>
2016-02-14 20:42:49 +0100

    2.4 Understanding scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,16 @@
 angular.module('todoListApp', [])
 .controller('mainCtrl', function($scope) {
   $scope.helloWorld = function() {
-    console.log('Hello there from the ctrl');
+    console.log('Hello there from the main ctrl');
+  }
+})
+
+.controller('coolCtrl', function($scope) {
+  $scope.whoAmI = function() {
+    console.log('Hello there from the cool ctrl');
+  }
+
+  $scope.helloWorld = function() {
+    console.log('Hello there from the cool ctrl ovveriden method');
   }
 });
