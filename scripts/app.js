95e6526dba1729c448ad3a542605f2c9fd4ba01c
Peter Toth <tothpeter08@gmail.com>
2016-02-15 08:23:26 +0100

    4.2 Using services to get data

--- scripts/app.js
+++ scripts/app.js
@@ -2,21 +2,17 @@ angular.module('todoListApp', [])
 .controller('mainCtrl', function($scope, dataService) {
   $scope.helloConsole = dataService.helloConsole;
 
-  $scope.learningNgChange = function() {
-    console.log('An input changed!');
-  }
-
-  $scope.todos = [
-    { name: 'clean the house' },
-    { name: 'water the lawn' },
-    { name: 'feed the dog' },
-    { name: 'pay dem bills' },
-    { name: 'run' },
-    { name: 'swim' }
-  ]
+  dataService.getTodos(function(response) {
+    $scope.todos = response.data;
+  });
 })
-.service('dataService', function() {
+.service('dataService', function($http) {
   this.helloConsole = function() {
     console.log('Hello from service!');
   }
+
+  this.getTodos = function(callback) {
+    $http.get('/mocks/todos.json')
+    .then(callback);
+  };
 });
